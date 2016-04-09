31cda7891f4c5f8f4506cc44c88ab2cba7dcce43
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-09 14:42:30 +0200

    Different debug layers

--- editor.js
+++ editor.js
@@ -27,12 +27,34 @@ var dbgeo = require("dbgeo");
 var pgp = require('pg-promise')();
 var db = pgp(cn);
 
-function createLineLayer() {
-
+function createLineLayer(id, source) {
+	return {
+		"id": id,
+        "type": "line",
+        "source": source,
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": randomColor(),
+            "line-width": 2
+        },
+		"filter": [	"all", ["==", "$type", "LineString" ]]
+	};
 }
 
-function createPointLayer() {
-
+function createPointLayer(id, source) {
+	return {
+        "id": id,
+        "type": "circle",
+        "source": source,
+        "paint": {
+            "circle-radius": 5,
+            "circle-color": randomColor(),
+        },
+		"filter": [	"all", ["==", "$type", "Point" ]]
+    };
 }
 
 function createPolygonLayer(id, source) {
@@ -43,10 +65,26 @@ function createPolygonLayer(id, source) {
         "paint": {
             "fill-color": randomColor(),
             "fill-opacity": 0.8
-        }
+        },
+		"filter": [	"all", ["==", "$type", "Polygon" ]]
     };
 }
 
+function createDebugLayers(map, id, source) {
+	try {
+		map.removeLayer(id + "_line");
+		map.removeLayer(id + "_point");
+		map.removeLayer(id + "_polygon");
+	} catch(err) {
+		//Layer don't exist yet
+	}
+
+	map.addLayer(createLineLayer(id + "_line", source));
+	map.addLayer(createPointLayer(id + "_point", source));
+	map.addLayer(createPolygonLayer(id + "_polygon", source));
+};
+
+var layers = [];
 function runAndDisplayQuery(subquery) {
     var transformQuery = "select ST_AsGeoJSON(ST_Transform(geometry, 4326)) AS geom, t.* from (" + subquery + ") as t";
     console.log(transformQuery);
@@ -61,12 +99,19 @@ function runAndDisplayQuery(subquery) {
                 return console.log(error);
             }
 
-            map.addSource("markers", {
-                "type": "geojson",
-                "data": result
-            });
-
-            map.addLayer(createPolygonLayer("markers_1", "markers"));
+			map.batch(function(batch) {
+				var sourceId = "query";
+				layers.forEach(function(i, l) {
+					map.removeSource(sourceId);
+				});
+				layers = [];
+				map.addSource("query", {
+					"type": "geojson",
+					"data": result
+				});
+				layers.push(sourceId);
+				createDebugLayers(batch, "debug", sourceId);
+			});
         });
     }).catch(function (error) {
         console.log(error);
