ceb5a44d4785d3ea1c94c7d84f84a1137e9e82d0
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-10 20:31:06 +0200

    Add modal and style it

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 var dbTestConn = {
     host: '192.168.99.100',
@@ -9,19 +10,41 @@ var dbTestConn = {
     password: 'osm'
 };
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+
 export class Toolbar extends React.Component {
     constructor(props) {
         super(props);
 		this.runQuery = this.runQuery.bind(this);
+		this.saveConnection = this.saveConnection(this);
 		this.connectDatabase = this.connectDatabase.bind(this);
+		this.state = {
+			editConnection: false
+		};
     }
 
     connectDatabase() {
+		this.setState({editConnection: true});
         this.props.db.connect(dbTestConn);
     }
 
     runQuery() {
 		window.events.publish('runQuery', {});
+    }
+
+    saveConnection() {
+		//TODO: Find better name for editConnection
+		this.setState({editConnection: false});
     }
 
 	render() {
@@ -34,6 +57,15 @@ export class Toolbar extends React.Component {
 				<i className="toolbar-icon fa fa-database"></i>
 				<span>Connect</span>
 			</button>
+			<Modal
+			  isOpen={this.state.editConnection}
+			  className="dark-modal"
+			  overlayClassName="dark-overlay"
+			  onRequestClose={this.saveConnection}
+			>
+			  <h1>Hi Guys! Please set the connection details</h1>
+			</Modal>
+
 		</div>;
 	}
 }
