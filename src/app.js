b13ab81b99dc49d486fd62067f56651a10e78208
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-10 21:11:03 +0200

    Prototype error handling modal

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import {Database} from './database.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Mediator} from 'mediator-js';
+import Modal from 'react-modal';
 import Codemirror from 'react-codemirror';
 
 
@@ -12,6 +13,27 @@ class App extends React.Component {
     constructor() {
         super();
         this.db = new Database();
+        this.closeErrorModal = this.closeErrorModal.bind(this);
+        this.state = {
+            errorMessage: '',
+            showError: false
+        };
+    }
+
+    componentDidMount() {
+        var self = this;
+        window.onerror = function(message, source, lineno, colno, error) {
+		    self.setState({
+                errorMessage: message,
+                showErrorModal: true
+            });
+        }
+    }
+
+    closeErrorModal() {
+        this.setState({
+            showErrorModal: false
+        });
     }
 
 	render() {
@@ -19,6 +41,14 @@ class App extends React.Component {
 			<Toolbar db={this.db} />
 			<Map />
 			<Editor db={this.db} />
+			<Modal
+			  isOpen={this.state.showErrorModal}
+			  className="dark-modal"
+			  overlayClassName="dark-overlay"
+			>
+			  <p>{this.state.errorMessage}</p>
+              <button onClick={this.closeErrorModal}>Close Modal...</button>
+			</Modal>
 		</div>;
 	}
 }
