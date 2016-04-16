3f5d6b63cb4c7c1106d2c98ce48b945fd092a614
lukasmartinelli <me@lukasmartinelli.ch>
2016-04-16 21:18:32 +0200

    Very crucial geometry column detection

--- src/database.js
+++ src/database.js
@@ -10,38 +10,66 @@ export class Database {
         // We can give much better error messages if we try to parse it
         // before we add our custom SQL transforms
         try {
-            var ast = parser.parse(lexer.tokenize(subquery));
+            parser.parse(lexer.tokenize(subquery));
         } catch(error) {
             errorHandler(error);
             return;
         }
 
-        //TODO: Use promises instead of callback
-        var transformQuery = "select ST_AsGeoJSON(ST_Transform(geometry, 4326)) AS geom, t.* from (" + subquery + ") as t";
-        this.db.result(transformQuery, true).then((result) => {
-            dbgeo.parse({
-                "data": result.rows,
-                "outputFormat": "geojson",
-                "geometryColumn": "geom",
-                "geometryType": "geojson"
-            }, (err, geojson) => {
-                console.log(result);
-		        window.events.publish('query.success', `${result.rowCount} rows in ${result.duration}ms`);
-                //TODO: This is solved some pattern somehow
-                cb(err, {
-                    geojson: geojson,
-                    rowCount: result.rowCount,
-                    fields: result.fields,
-                    command: result.command,
+        this.transformQuery(subquery).then(({query, geomField}) => {
+            return this.db.result(query, true).then((result) => {
+                dbgeo.parse({
+                    "data": result.rows,
+                    "outputFormat": "geojson",
+                    "geometryColumn": geomField.name,
+                    "geometryType": "geojson"
+                }, (err, geojson) => {
+                    console.log(result);
+                    window.events.publish('query.success', `${result.rowCount} rows in ${result.duration}ms`);
+                    //TODO: This is solved some pattern somehow
+                    cb(err, {
+                        geojson: geojson,
+                        rowCount: result.rowCount,
+                        fields: result.fields,
+                        command: result.command,
+                    });
                 });
             });
         }).catch(errorHandler);
+    }
+
+    transformQuery(query) {
+        var limitQuery = `select * from (${query}) as t limit 1`;
+        return this.db.result(limitQuery, true).then(result => {
+            const firstGeomField = result.fields.filter(isGeometryField).shift();
+            const selectClause = result.fields.map(field => {
+                if(isGeometryField(field)) {
+                    return `ST_AsGeoJSON(ST_Transform(${field.name}, 4326)) as ${field.name}`
+                }
+                return field.name;
+            }).join(',');
+
+            if(!firstGeomField) throw Error('No geometry column found in query');
+            return {
+                query: `SELECT ${selectClause} FROM (${query}) as t`,
+                geomField: firstGeomField
+            };
+        });
     }
 
     connect(connectionOptions) {
         this.db = pgp(connectionOptions);
         return this.db.connect();
     }
+}
+
+function isGeometryField(field) {
+    return (
+        field.dataTypeID === 16391 ||
+        field.name === 'geom' ||
+        field.name === 'wkb_geometry' ||
+        field.name === 'geometry'
+    );
 }
 
 export function isGeometry(fieldName) {
