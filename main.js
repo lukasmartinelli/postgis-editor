33c2f18c79e94d952bd45c19fa87127f47948532
lukasmartinelli <me@lukasmartinelli.ch>
2016-07-13 23:36:43 +0200

    Electron upgrade

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-
-// Report crashes to our server.
-require('crash-reporter').start();
+var electron = require('electron')
+var app = electron.app;  // Module to control application life.
+var BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -20,15 +18,14 @@ app.on('window-all-closed', function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 1000, height: 800});
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 800});
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
